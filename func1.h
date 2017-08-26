d096e1873365dd1099e4c4a37b60c7a4157eeefe
Nik8x <30778529+Nik8x@users.noreply.github.com>
2017-08-26 13:21:26 -0400

    func1.h
    header files are here

---
+++ func1.h
@@ -0,0 +1,35 @@
+#include<stdio.h>	//all the he der files included in this file 
+#include<conio.h>
+#include<iostream.h>
+#include <dos.h>
+#include <time.h>
+#include<process.h>
+#include<string.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+#define UP 0x48
+#define down 0x50
+#define Enter 0xd
+#define Esc 0x1b
+
+int menu(int,int,int);
+void display_time(int,int) ;
+
+#ifndef_func1_h
+#define_func1_h
+
+//next is time generator
+
+class time1
+{
+public:
+timer(int,int);
+
+};
+
+void display_time(int,int);           // IT DISPLAYS THE TIME
+#include"func1.cpp"			  // ALL THE FUNCTION ARE DEFIE IN THIS FILE 
+#endif
